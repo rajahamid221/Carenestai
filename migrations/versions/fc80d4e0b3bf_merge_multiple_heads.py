7df232a3cbe3e3686ff4e9f0f23265b261b65346
Hamid Javed <hamid.javed@fff>
2025-05-08 15:16:51 +0500

    Fix database migrations: Remove duplicate doctor_fields migration and add goal_id foreign key

--- migrations/versions/fc80d4e0b3bf_merge_multiple_heads.py
+++ migrations/versions/fc80d4e0b3bf_merge_multiple_heads.py
@@ -1,7 +1,7 @@
 """Merge multiple heads
 
 Revision ID: fc80d4e0b3bf
-Revises: add_doctor_fields, c39aabddb579
+Revises: bb30007db09f, c39aabddb579
 Create Date: 2025-04-28 15:20:18.719440
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'fc80d4e0b3bf'
-down_revision = ('add_doctor_fields', 'c39aabddb579')
+down_revision = ('bb30007db09f', 'c39aabddb579')
 branch_labels = None
 depends_on = None
 
